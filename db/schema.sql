8290ab555df63fd5a70a68cce56149b9a512c92d
tielkingc <tielking5@gmail.com>
2021-07-01 12:39:46 -0400

    add constraints

--- db/schema.sql
+++ db/schema.sql
@@ -2,22 +2,31 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
 
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary INTEGER NOT NULL,
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department_id
+        FOREIGN KEY (department_id)
+        REFERENCES departments(id)
+);
+
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER NOT NULL
-);
-
-CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary INTEGER NOT NULL,
-    department_id INTEGER NOT NULL
-);
-
-CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    manager_id INTEGER NOT NULL,
+    CONSTRAINT fk_role_id
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id),
+    CONSTRAINT fk_manager_id
+        FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
 );
