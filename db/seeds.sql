2787f7a132a5e7f0553d4e8b388238704290fb9e
tielkingc <tielking5@gmail.com>
2021-07-01 14:03:07 -0400

    added department routes

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,13 +5,13 @@ VALUES
     ('Susan', 'Clark', 002, 010),
     ('James', 'Newman', 010, 010);
 
-INSERT INTO role
+INSERT INTO roles
     (title, salary, department_id)
 VALUES
     ('Developer', 60000, 01),
     ('Manager', 80000, 01);
 
-INSERT INTO department
+INSERT INTO departments
     (name)
 VALUES
     ('Software Support');
